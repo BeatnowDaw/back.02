0733c9cbaae4abdb2260ae6f11e4675c88931bab
Hugo Garcia <99070807+hugogaarciaa@users.noreply.github.com>
2024-04-11 15:51:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ import uvicorn
 app = FastAPI()
 app.include_router(router)
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
